2ea425bef9026aca4f1065e8b3e6cea9802a7817
Andreas Nilsson <andreas.l.nilsson@evry.com>
2019-11-08 17:43:29 +0100

    Updates

---
+++ precache-manifest.4dfec6d62ac04fdbfbb77301988d621d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78490b96e79062dcadca9e2607978fee",
+    "url": "/flipcard/index.html"
+  },
+  {
+    "revision": "01ae4e819d074978be32",
+    "url": "/flipcard/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "48a5c8ed2cd0de1e23a2",
+    "url": "/flipcard/static/js/2.4153d0bc.chunk.js"
+  },
+  {
+    "revision": "01ae4e819d074978be32",
+    "url": "/flipcard/static/js/main.b03c96c1.chunk.js"
+  },
+  {
+    "revision": "30f715897164f0f3acc2",
+    "url": "/flipcard/static/js/runtime-main.526e6fda.js"
+  }
+]);
